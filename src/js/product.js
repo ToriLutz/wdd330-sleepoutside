6da64e1eac21209a55bf44c98759eef50ffc3a4b
ToriLutz <vglutz@comcast.net>
2025-09-05 18:25:50 -0700

    fixed

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,10 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; 
+  let cartItems = getLocalStorage("so-cart");
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
